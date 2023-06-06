23d8ec9ab03829407562318461784a8ecad42812
Cecilio Silva Monteiro <csilvamon@logi-app.nl>
2023-06-06 15:40:13 +0200

    Added dummy route

---
+++ src/main/java/me/ceciliosilva/ipass/mealmaster/webservices/MealResource.java
@@ -0,0 +1,16 @@
+package me.ceciliosilva.ipass.mealmaster.webservices;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("meals")
+public class MealResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMeal() {
+        return "Wow it works and good too";
+    }
+}
